f1d61234368b26ccc8de3a09318be21975dcadc1
Govin <timsinagovin17@gmail.com>
2023-09-21 16:51:34 +0545

    codes updated

--- index.js
+++ index.js
@@ -8,33 +8,24 @@ let finalDest;
 
 http
   .createServer((req, res) => {
-    //routing without using express
     switch (req.url) {
       case "/":
-        console.log(req.url);
         finalDest = path.join(htmlPaths, "index.html");
-        res.statusCode = 200;
-        res.statusMessage = "errrrr";
         break;
       case "/about":
         finalDest = path.join(htmlPaths, "about.html");
-        res.statusCode = 200;
-        res.statusMessage = "Successful return of about page";
-        res.end();
         break;
       case "/about-us":
-        res.statusCode = 304;
+        res.statusCode = 302;
         res.setHeader("Location", "/about");
         res.end();
         break;
       default:
         finalDest = path.join(htmlPaths, "notFound.html");
         res.statusCode = 404;
-        res.statusMessage = "errrrr";
         break;
     }
-
-    fs.readFile(`${finalDest}`, (err, data) => {
+    fs.readFile(finalDest, (err, data) => {
       if (err) {
         console.log(err);
         res.end();
